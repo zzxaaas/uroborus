4af608a9341f4fb048a12bce98d891eaf920918d
zihan chen <chenzihan@momenta.ai>
2021-11-29 21:12:09 +0800

    feat:login

--- service/docker.go
+++ service/docker.go
@@ -1,7 +1,12 @@
 package service
 
 import (
+	"context"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
+	"io"
+	"os"
 )
 
 type DockerService struct {
@@ -14,11 +19,28 @@ func NewDockerService(cli *client.Client) *DockerService {
 	}
 }
 
-//func (s DockerService) PullImage(image string) error {
-//	ctx := context.Background()
-//	reader,err := s.cli.ImagePull(ctx,image,types.ImagePullOptions{})
-//	if err != nil {
-//		return err
-//	}
-//	io.Copy(os.Stdout,reader)
-//}
+func (s DockerService) PullImage(image string) error {
+	ctx := context.Background()
+	reader, err := s.cli.ImagePull(ctx, image, types.ImagePullOptions{})
+	if err != nil {
+		return err
+	}
+	io.Copy(os.Stdout, reader)
+	return nil
+}
+
+func (s DockerService) StartContainer(image, containerName string) {
+	ctx := context.Background()
+
+	resp, err := s.cli.ContainerCreate(ctx, &container.Config{
+		Image: image,
+	}, nil, nil, nil, containerName)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := s.cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+}
